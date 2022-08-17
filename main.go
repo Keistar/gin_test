57e264fbb623810d721051963c512a60f07cd3d8
Keistar <keistar0926@gmail.com>
2022-08-17 10:46:00 +0900

    リファクタリング

--- main.go
+++ main.go
@@ -7,11 +7,13 @@ import (
 
 func main() {
 	engine := gin.Default()
-	// htmlのディレクトリを指定
+
+	// Road HTML
 	engine.LoadHTMLGlob("templates/*")
+
+	// Routing
 	engine.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl", gin.H{
-			// htmlに渡す変数を定義
 			"message": "hello gin",
 		})
 	})
